7d6473cb83ade199a66a16a9c8acc2ef359fbfd9
Peter Sanchez <peter@netlandish.com>
2017-01-11 13:00:58 -0800

    Updated for more obvious field naming and admin usage

--- simpler_faq/admin.py
+++ simpler_faq/admin.py
@@ -3,14 +3,16 @@ from django.contrib import admin
 from .models import Topic, Question
 
 
-class FAQAdmin(admin.ModelAdmin):
-    list_display = ('id', 'text', 'number')
-    list_editable = ('number',)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text', 'order')
+    list_editable = ('order',)
 
 
-class QuestionAdmin(FAQAdmin):
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text', 'topic', 'order')
+    list_editable = ('topic', 'order',)
     list_filter = ('topic',)
 
 
-admin.site.register(Topic, FAQAdmin)
+admin.site.register(Topic, TopicAdmin)
 admin.site.register(Question, QuestionAdmin)
--- simpler_faq/models.py
+++ simpler_faq/models.py
@@ -4,22 +4,22 @@ from django.utils.translation import ugettext_lazy as _
 
 class Topic(models.Model):
     text = models.CharField(max_length=200)
-    number = models.IntegerField()
+    order = models.PositiveIntegerField()
 
     class Meta:
         verbose_name = _('Topic')
         verbose_name_plural = _('Topics')
-        ordering = ['number']
+        ordering = ['order']
 
     def __unicode__(self):
-        return u'(%s) %s' % (self.number, self.text, )
+        return u'({0}) {1}'.format(self.order, self.text)
 
 
 class Question(models.Model):
     text = models.CharField(max_length=200)
     answer_text = models.TextField()
     topic = models.ForeignKey(Topic, related_name='questions')
-    number = models.IntegerField()
+    order = models.IntegerField()
     related_questions = models.ManyToManyField(
         'self',
         related_name='related_questions',
@@ -29,7 +29,7 @@ class Question(models.Model):
     class Meta:
         verbose_name = _('Question')
         verbose_name_plural = _('Questions')
-        ordering = ('number',)
+        ordering = ('order',)
 
     def __unicode__(self):
-        return u'(%s) %s' % (self.number, self.text, )
+        return u'({0}) {1}'.format(self.order, self.text)
